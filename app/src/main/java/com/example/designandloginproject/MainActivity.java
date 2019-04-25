db1804eecb2923629cbbc91ef228e91b3086896d
rimseeh <rajaie311@gmail.com>
2019-04-25 13:01:13 +0300

    4/25/2019 end of day

--- app/src/main/java/com/example/designandloginproject/MainActivity.java
+++ app/src/main/java/com/example/designandloginproject/MainActivity.java
@@ -4,13 +4,21 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.designandloginproject.models.Accessory;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements NavigationHost{
 
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayList<Accessory> accessories = (ArrayList<Accessory>) Accessory.initAccessoryEntryList(getResources());
+        Log.d(TAG, "onCreate: "+accessories);
         if (savedInstanceState == null) {
             getSupportFragmentManager()
                     .beginTransaction()
--- app/src/main/java/com/example/designandloginproject/models/Accessory.java
+++ app/src/main/java/com/example/designandloginproject/models/Accessory.java
@@ -1,16 +1,40 @@
 package com.example.designandloginproject.models;
 
+import android.content.res.Resources;
+import android.net.Uri;
+import android.util.Log;
+
+import com.example.designandloginproject.R;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Accessory {
+
+    private static final String TAG = "Accessory";
     private String id;
     private String title;
     private String description;
     private String price;
     private String url;
+    private Uri dynamicUrl;
 
-    public Accessory(String title, String description, String price, String url) {
+
+    public Accessory(String title, String description, String price, String url,String dynamicUrl) {
         this.title = title;
         this.description = description;
         this.price = price;
+        this.dynamicUrl = Uri.parse(dynamicUrl);
         this.url = url;
     }
 
@@ -58,6 +82,14 @@ public class Accessory {
         this.url = url;
     }
 
+    public Uri getDynamicUrl() {
+        return dynamicUrl;
+    }
+
+    public void setDynamicUrl(Uri dynamicUrl) {
+        this.dynamicUrl = dynamicUrl;
+    }
+
     @Override
     public String toString() {
         return "Accessory{" +
@@ -66,6 +98,35 @@ public class Accessory {
                 ", description='" + description + '\'' +
                 ", price='" + price + '\'' +
                 ", url='" + url + '\'' +
+                ", dynamicUrl=" + dynamicUrl +
                 '}';
     }
+
+    //ProductEntry.initProductEntryList(getResources())
+    public static List<Accessory> initAccessoryEntryList(Resources resources) {
+        InputStream inputStream = resources.openRawResource(R.raw.products);
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+            int pointer;
+            while ((pointer = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, pointer);
+            }
+        } catch (IOException exception) {
+            Log.e(TAG, "Error writing/reading from the JSON file.", exception);
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException exception) {
+                Log.e(TAG, "Error closing the input stream.", exception);
+            }
+        }
+        String jsonProductsString = writer.toString();
+        Gson gson = new Gson();
+        Type productListType = new TypeToken<ArrayList<Accessory>>() {
+        }.getType();
+        return gson.fromJson(jsonProductsString, productListType);
+    }
 }
+
